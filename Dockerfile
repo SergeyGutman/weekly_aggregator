df30d52210c85ab40517180f7a60a9985cc9ea34
Sergey Gutman <sergutman4@gmail.com>
2024-09-23 20:37:06 +0300

    Add spark_airflow project

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM apache/airflow:2.7.1-python3.11
+
+USER root
+RUN apt-get update && \
+    apt-get install -y gcc python3-dev openjdk-11-jdk openjdk-11-jre && \
+    apt-get clean
+# Установите переменную окружения JAVA_HOME
+ENV JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
+ENV PATH $PATH:$JAVA_HOME/bin  
+USER airflow
+
+# Установите совместимые версии провайдеров
+RUN pip install --upgrade pip
+RUN pip install apache-airflow-providers-apache-spark==3.0.0 pyspark
+
---
+++ src/main/scala/CsvAggregator.scala
@@ -0,0 +1,83 @@
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.nio.file.{Files, Paths}
+
+object Aggregator {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("CSV Aggregator")
+      .master("spark://spark-master:7077")
+      .getOrCreate()
+      
+
+    val inputPath = "input/"
+    val intermediatePath = "intermediate/"
+    val outputPath = "output/"
+    val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    // Получаем текущую дату
+    val currentDate = LocalDate.now()
+    
+    // Обрабатываем последние 7 дней
+    for (i <- 1 until 8) {
+      val date = currentDate.minusDays(i)
+      val dateString = date.format(dateFormat)
+      val filePath = s"$inputPath$dateString.csv"
+      val intermediateFilePath = s"$intermediatePath$dateString.csv"
+
+      // Проверяем, существует ли файл intermediate, и если его нет агрегируем и создаем
+      if (!Files.exists(Paths.get(intermediateFilePath))) {
+        // Читаем CSV файл
+        val df = spark.read.option("header", "false").csv(filePath)
+        val aggregatedDF = aggregateData(df)
+
+        // Сохраняем агрегированные данные        
+        aggregatedDF.write.option("header", "true").csv(intermediateFilePath)        
+      }
+    }
+
+    // Агрегируем данные из промежуточных файлов
+    val aggregatedOutputDF = aggregateIntermediateFiles(spark, currentDate, dateFormat, intermediatePath)
+    
+    // Сохраняем общий агрегированный файл
+    aggregatedOutputDF.write.option("header", "true").csv(s"$outputPath${currentDate.format(dateFormat)}.csv")
+
+    spark.stop()
+  }
+
+  def aggregateData(df: DataFrame): DataFrame = {
+    df.withColumnRenamed("_c0", "email")
+      .withColumnRenamed("_c1", "action")
+      .groupBy("email")
+      .agg(
+        sum(when(col("action") === "CREATE", 1).otherwise(0)).alias("create_count"),
+        sum(when(col("action") === "READ", 1).otherwise(0)).alias("read_count"),
+        sum(when(col("action") === "UPDATE", 1).otherwise(0)).alias("update_count"),
+        sum(when(col("action") === "DELETE", 1).otherwise(0)).alias("delete_count")
+      )
+  }
+
+  def aggregateIntermediateFiles(spark: SparkSession, currentDate: LocalDate, dateFormat: DateTimeFormatter, intermediatePath: String): DataFrame = {
+    val aggregatedDFs = (1 until 8).map { i =>
+      val date = currentDate.minusDays(i)
+      val dateString = date.format(dateFormat)
+      val filePath = s"$intermediatePath$dateString.csv"
+      if (Files.exists(Paths.get(filePath))) {
+        spark.read.option("header", "true").csv(filePath)
+      } else {
+        spark.emptyDataFrame
+      }
+    }
+
+    // Объединяем все DataFrame и агрегируем
+    aggregatedDFs.reduce(_ union _).groupBy("email")
+      .agg(
+        sum("create_count").alias("create_count"),
+        sum("read_count").alias("read_count"),
+        sum("update_count").alias("update_count"),
+        sum("delete_count").alias("delete_count")
+      )
+  }
+}
